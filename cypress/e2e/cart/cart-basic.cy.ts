e1164994e006c3a5755eaab638c1741adf78bb69
Valerie_Bekker <mion.s.vocaloid@gmail.com>
2026-03-11 14:17:40 +0400

    feat: implement cart basic functionality tests

--- cypress/e2e/cart/cart-basic.cy.ts
+++ cypress/e2e/cart/cart-basic.cy.ts
@@ -13,9 +13,32 @@ describe('Cart Basic Functionality Tests', () => {
     })
 
     it('should successfully add a product to the cart', () => {
-        const firstProductName = productsPage.getProductNameByIndex(0);
-        productsPage.clickAddToCartBtnByIndex(0);
-        productsPage.clickCartHeaderBtn();
-        cartPage.verifyProductNameInList(firstProductName);
+    productsPage.getProductNameByIndex(0).then((firstProductName) => {
+    productsPage.clickAddToCartBtnByIndex(0)
+    productsPage.clickCartHeaderBtn()
+    cartPage.verifyProductNameInCart(firstProductName, true)
     })
+    })
+
+   it('should successfully remove a product from the cart', () => {
+  productsPage.getProductNameByIndex(0).then((firstProductName) => {
+    productsPage.clickAddToCartBtnByIndex(0)
+    productsPage.clickCartHeaderBtn()
+    cartPage.verifyProductNameInCart(firstProductName, true)
+    cartPage.clickRemoveBtn()
+    cy.contains('Removed from cart').should('be.visible')
+    cartPage.verifyProductNameInCart(firstProductName, false)
+  })
+})
+
+   it('should update product quantity in the cart correctly', () => {
+  productsPage.getProductNameByIndex(0).then((firstProductName) => {
+    productsPage.clickAddToCartBtnByIndex(0)
+    productsPage.clickCartHeaderBtn()
+    cartPage.verifyProductNameInCart(firstProductName, true)
+    cartPage.verifyQuantity(1);
+    cartPage.clickQuantityPlusButton(2);
+    cartPage.verifyQuantity(3);
+  })
+})
 })
--- cypress/pages/cart.page.ts
+++ cypress/pages/cart.page.ts
@@ -2,13 +2,33 @@ class CartPage {
 
     elements = {
     productNameInCart: (productName: string) => cy.contains(productName),
+    removeButton: () => cy.contains('Remove'),
+    quantityPlusButton: () => cy.contains('+')
     }
 
-    verifyProductNameInList(productName: Cypress.Chainable<string>): void {
-    productName.then((name) => {
-    cy.contains(name).should('be.visible')
-    })
+verifyProductNameInCart(productName: string, shouldBeVisible: boolean): void {
+    cy.contains(productName).should(shouldBeVisible ? 'be.visible' : 'not.exist')
+  }
+
+clickRemoveBtn(): void {
+    this.elements.removeButton().click();
+}
+
+clickQuantityPlusButton(numberOfClicks: number): void {
+    for (let i = 0; i < numberOfClicks; i++) {
+        this.elements.quantityPlusButton().click();
     }
+}
+
+verifyQuantity(quantityOfItems: number): void {
+cy.contains('Quantity')
+  .parent()
+  .find('span')
+  .invoke('text')
+  .then((quantityText) => {
+    expect(quantityText.trim()).to.eq(quantityOfItems.toString());
+  })
+}
 
 }
 
